9e35f9b898983b6126b05e4b97a3c8ebe3e1c81d
윤성노 <m3114@naver.com>
2021-12-04 11:43:50 +0900

    프래그먼트의 동자 관련 코드 onActivitycreated 함수 오버라이딩 화면을 전달할때 this대신 reQuirecontext를 한다

---
+++ app/src/main/java/com/neppplus/fragment_20211204/fragments/firstFragment.kt
@@ -0,0 +1,31 @@
+package com.neppplus.fragment_20211204.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.neppplus.fragment_20211204.R
+import kotlinx.android.synthetic.main.fragment_first.*
+
+class firstFragment : Fragment(){
+//  xml을 연결해주는 역할만 담당.
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout,fragment, container,false)
+    }
+//동작 관련 코드 모음
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        
+    btnToast.setOnClickListener {
+        Toast.makeText(requireContext(), "프레금머트에서 토스트 띄우기", Toast.LENGTH_SHORT).show()
+    }
+    
+    }
+            
+}
